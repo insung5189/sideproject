dddb505b1e4988cc36cb74efa4a81944dadb638a
insung5189 <insung5189@gmail.com>
2025-02-04 23:25:21 +0900

    git init

---
+++ src/test/java/com/ll/sideproject/SideprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sideproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
