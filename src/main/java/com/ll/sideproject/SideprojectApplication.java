f64987a27f93f72c55e0827b1fe4a9e9c3d430f9
insung5189 <insung5189@gmail.com>
2025-03-02 18:59:01 +0900

    application.yml 설정 로컬과 개발환경 분리

--- src/main/java/com/ll/sideproject/SideprojectApplication.java
+++ src/main/java/com/ll/sideproject/SideprojectApplication.java
@@ -1,5 +1,6 @@
 package com.ll.sideproject;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SideprojectApplication {
 
 	public static void main(String[] args) {
+		// .env 파일 로드
+		Dotenv dotenv = Dotenv.load();
+
+		// 환경변수 설정
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(SideprojectApplication.class, args);
 	}
 
