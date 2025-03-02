8c30dbf117be6e1f0abc0d100a76015e5a994c5e
insung5189 <insung5189@gmail.com>
2025-03-02 18:59:17 +0900

    홈 컨트롤러 생성(임시 메인페이지)

--- src/main/java/com/ll/sideproject/global/controller/HomeController.java
+++ src/main/java/com/ll/sideproject/global/controller/HomeController.java
@@ -1,13 +1,16 @@
-package com.ll.sideproject.global.controller;
+package com.ll.sideproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "Welcome to the Article Board API!";
+    public String home(Model model) {
+        String welcomeMsg = "Welcome to the Sample Homepage!";
+        model.addAttribute("message", welcomeMsg);
+        return "home"; // home.html 파일을 렌더링
     }
 }
